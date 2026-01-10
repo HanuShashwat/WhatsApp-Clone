7e84f6201a4fe0d092edf9ff9c8042cf3c77baed
Hanu Shashwat <hanushashwat733@gmail.com>
2026-01-10 16:22:19 +0530

    Built Contact list for the Web Layout & removed few bugs

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/contacts_list.dart';
 
 class WebScreenLayout extends StatelessWidget {
   const WebScreenLayout({super.key});
@@ -6,9 +7,32 @@ class WebScreenLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("This is web."),
-      ),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Web Profile Bar
+                  // Web Search List
+                  ContactsList(),
+                ],
+              ),
+            ),
+          ),
+          // Web Screen
+          Container(
+            width: MediaQuery.of(context).size.width*0.75,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backgroundImage.png'),
+                fit: BoxFit.cover,
+              )
+            ),
+          )
+        ],
+      )
     );
   }
 }
--- lib/widgets/contacts_list.dart
+++ lib/widgets/contacts_list.dart
@@ -7,60 +7,58 @@ class ContactsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: Padding(padding: EdgeInsets.only(top: 10),
-        child: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: info.length,
-                itemBuilder: (context, index) {
-                  return InkWell(
-                    onTap: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 8.0),
-                      child: ListTile(
-                        title: Text(
-                          info[index]['name'].toString(),
-                          style: const TextStyle(
+    return Padding(
+      padding: EdgeInsets.only(top: 10),
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: info.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                InkWell(
+                  onTap: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: ListTile(
+                      title: Text(
+                        info[index]['name'].toString(),
+                        style: const TextStyle(
                             fontSize: 18
-                          ),
                         ),
-                        subtitle: Padding(
-                          padding: EdgeInsets.only(top: 6),
-                          child: Text(
-                            info[index]['message'].toString(),
-                            style: const TextStyle(
+                      ),
+                      subtitle: Padding(
+                        padding: EdgeInsets.only(top: 6),
+                        child: Text(
+                          info[index]['message'].toString(),
+                          style: const TextStyle(
                               fontSize: 15
-                            ),
                           ),
                         ),
-                        leading: CircleAvatar(
-                          backgroundImage: NetworkImage(
-                              info[index]['profilePic'].toString()
-                          ),
+                      ),
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: NetworkImage(
+                            info[index]['profilePic'].toString()
                         ),
-                        trailing: Text(
-                            info[index]['time'].toString(),
-                            style: const TextStyle(
-                                fontSize: 13,
-                                color: Colors.grey
-                            ),
+                      ),
+                      trailing: Text(
+                        info[index]['time'].toString(),
+                        style: const TextStyle(
+                            fontSize: 13,
+                            color: Colors.grey
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
-            ),
-            const Divider(
-              color: dividerColor,
-              indent: 85,
-            )
-          ],
+                  ),
+                ),
+                const Divider(
+                  color: dividerColor,
+                  indent: 85,
+                ),
+              ],
+            );
+          },
         ),
-      ),
     );
   }
 }
