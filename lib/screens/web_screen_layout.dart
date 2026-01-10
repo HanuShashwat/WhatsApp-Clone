268610d02aa6156f7deee7310b7d9ad1b10aa51e
Hanu Shashwat <hanushashwat733@gmail.com>
2026-01-10 21:18:00 +0530

    Built Web Chat Appbar

--- lib/screens/web_screen_layout.dart
+++ lib/screens/web_screen_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
+import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
 import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
@@ -31,6 +32,13 @@ class WebScreenLayout extends StatelessWidget {
                 image: AssetImage('assets/backgroundImage.png'),
                 fit: BoxFit.cover,
               )
+            ),
+            child: Column(
+              children: [
+                WebChatAppbar(),
+                // Chat List
+                // Message Input Box
+              ],
             ),
           )
         ],
